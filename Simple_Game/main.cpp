624ef1e0ea46088da7ff7b301c4693cafaa1227d
Tracey King <traceyking@tracey-pc.gateway.2wire.net>
2014-03-02 19:58:07 -0500

    Initial Commit

---
+++ Simple_Game/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Simple_Game
+//
+//  Created by Tracey King on 3/2/14.
+//  Copyright (c) 2014 Tracey King. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
